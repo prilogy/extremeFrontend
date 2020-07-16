4a1d2b2568623ecc1d8f576bb92394bf1f10d325
ksydex <artglz63@gmail.com>
2020-07-16 21:22:04 +0300

    Moved redux store initialization to its file

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:extreme/styles/app_theme.dart';
 import 'package:extreme/styles/intents.dart';
 import 'package:extreme/widgets/nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:redux/redux.dart';
 //import 'package:flutter_svg/flutter_svg.dart';
 
 import 'accountPage.dart';
@@ -12,7 +13,7 @@ import 'helpers/interfaces.dart';
 import 'redux.dart';
 import 'screens/news_screen.dart';
 
-import 'package:redux/redux.dart';
+import './redux.dart' as Redux;
 import 'package:flutter_redux/flutter_redux.dart';
 
 const List<String> assetNames = <String>['svg/home.svg', 'svg/nothing.svg'];
@@ -21,7 +22,7 @@ const List<String> assetNames = <String>['svg/home.svg', 'svg/nothing.svg'];
 
 void main() async {
   // TODO: implement get initialState from rest API
-  final store = Store<Info>(infoReducer, initialState: Info(likesCount: 100));
+  //final store = Store<Info>(infoReducer, initialState: Info(likesCount: 100));
   final env = await EnvConfig.get("./.env");
   print(store.state.likesCount.toString()); // likesCount = 100
   runApp(MyApp(
@@ -40,7 +41,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     print('MyApp builder: ' + store.state.likesCount.toString());
     return StoreProvider<Info>(
-      store: store,
+      store: Redux.store,
       child: MaterialApp(
         title: 'Flutter App',
         theme: AppTheme.dark,
