0978103a359d9216eeb83e680d1a8b48b79bfcf1
Kirill <fairsuspect@gmail.com>
2020-08-05 14:42:53 +0300

    custom listview builder

--- lib/screens/playlists_screen.dart
+++ lib/screens/playlists_screen.dart
@@ -1,9 +1,12 @@
 import 'package:extreme/main.dart';
 import 'package:extreme/styles/intents.dart';
 import 'package:extreme/widgets/block_base_widget.dart';
+import 'package:extreme/widgets/list_view_widget.dart';
 import 'package:extreme/widgets/playlist_card.dart';
 import 'package:extreme/widgets/screen_base_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:extreme/models/main.dart' as Models;
+import 'package:extreme/services/api/main.dart' as Api;
 
 class Playlists extends StatelessWidget {
   //const Playlists({Key key}) : super(key: key);
@@ -14,48 +17,23 @@ class Playlists extends StatelessWidget {
       appBar: AppBar(title: Text('Плейлисты')),
       builder: (context) => [
         BlockBaseWidget(
-                //header: "Популярные плейлисты",
-                margin: EdgeInsets.zero,
-                child: ListView(
-                  shrinkWrap: true,
-                  primary: false,
-                  children: [
-                    for (var item in [
-                      PlayListCard(
-                        aspectRatio: 16 / 9,
-                        padding: EdgeInsets.only(bottom: Indents.md),
-                        isLiked: false,
-                      ),
-                      PlayListCard(
-                        aspectRatio: 16 / 9,
-                        padding: EdgeInsets.only(bottom: Indents.md),
-                        isLiked: true,
-                      ),
-                      PlayListCard(
-                        aspectRatio: 16 / 9,
-                        padding: EdgeInsets.only(bottom: Indents.md),
-                        isLiked: false,
-                      ),
-                      PlayListCard(
-                        aspectRatio: 16 / 9,
-                        padding: EdgeInsets.only(bottom: Indents.md),
-                        isLiked: true,
-                      ),
-                      PlayListCard(
-                        aspectRatio: 16 / 9,
-                        padding: EdgeInsets.only(bottom: Indents.md),
-                        isLiked: false,
-                      ),
-                      PlayListCard(
-                        aspectRatio: 16 / 9,
-                        padding: EdgeInsets.only(bottom: Indents.md),
-                        isLiked: true,
-                      ),
-                    ])
-                      item
-                  ],
-                ),
-              ),
+          //header: "Популярные плейлисты",
+          child: FutureBuilder(
+            future: Api.Entities.playlists(1, 4),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                return ListGenerator<Models.Playlist>(
+                  models: snapshot.data,
+                );
+              } else if (snapshot.hasError) {
+                return Text(snapshot.error.toString());
+              } else
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+            },
+          ),
+        ),
       ],
     );
   }
