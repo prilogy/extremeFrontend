ab0e8f3fbb03f4edcbf2165c16072256b25de334
Алексей <55784938+aolychkin@users.noreply.github.com>
2021-08-25 14:03:21 +0300

    push notifications ios config

--- flutter-pagination-helper/ios/Flutter/flutter_export_environment.sh
+++ flutter-pagination-helper/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/mak/Downloads/flutter-2"
-export "FLUTTER_APPLICATION_PATH=/Users/mak/Desktop/dev/extremeFrontend/flutter-pagination-helper"
+export "FLUTTER_ROOT=/Users/artemmurnaev/dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/artemmurnaev/Documents/extremeFrontend/flutter-pagination-helper"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
